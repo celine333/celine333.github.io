bba8395f767188869c26fb74b4fc51a9076bbb0b
celine333 <56337413+celine333@users.noreply.github.com>
2019-11-16 14:21:49 +0100

    Aufgabe5

---
+++ Aufgabe_5/Neu.js
@@ -0,0 +1,8 @@
+var test = "Hellu";
+var welt = "Welt";
+var zahl1 = 200;
+var zahl2 = 20;
+var ergebnis = zahl2 / zahl1 * 100;
+/*var prozent:string = "%";*/
+console.log(ergebnis +  % );
+//# sourceMappingURL=Neu.js.map
