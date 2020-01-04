a86385eeedfeece03bf35eb0c5e143e8d4c2d8d9
celine333 <56337413+celine333@users.noreply.github.com>
2020-01-04 15:31:08 +0100

    Steckbrief

--- Aufgabe_9/style.js
+++ Aufgabe_9/style.js
@@ -6,7 +6,7 @@ window.addEventListener("load", function () {
 function AddTaskText() {
     var newtask = document.querySelector(".newtask").value;
     console.log(newtask);
-    addTaskToList(newtask);
+    addTaskToList(newtask); // Aufruf der Funktion drunter
     // Counter:
     var element = document.querySelector(".Rahmen");
     var numberofChildren = element.children.length;
@@ -17,33 +17,24 @@ function AddTaskText() {
 function addTaskToList(newTaskValue) {
     var addElement = document.createElement("div");
     addElement.setAttribute("class", "Template");
-    addElement.setAttribute("id", "TaskId" + taskcounter);
-    var myHtmlTemplate = "";
+    addElement.setAttribute("id", "TaskId" + taskcounter); // Template konstruiert
+    var myHtmlTemplate = ""; // Template Inhalte
     myHtmlTemplate += "<button onclick=\"CheckedButton(this)\" class=\"far fa-circle checkbox\" id=\"circle" + taskcounter + "\"></button>";
     myHtmlTemplate += "<input class=\"firsttask\" id=\"firsttask" + taskcounter + "\" value=\"" + newTaskValue + "\" ></input>";
     myHtmlTemplate += "<button onclick=\"DeleteTaskText(this)\" class=\"far fa-trash-alt trash\" id=\"delete" + taskcounter + "\"></button>";
     addElement.innerHTML = myHtmlTemplate;
-    document.querySelector(".Rahmen").appendChild(addElement);
+    document.querySelector(".Rahmen").appendChild(addElement); // Hinzufügen vom Template
     console.log("addTasktoList:" + newTaskValue);
 }
 function DeleteTaskText(ClickedTrash) {
     console.log("This is deleating the task");
-    ClickedTrash.parentElement.remove();
+    ClickedTrash.parentElement.remove(); // div wird bei click auf Tonne gelöscht
     taskcounter--;
     // Counter
     var element = document.querySelector(".Rahmen");
     var numberofChildren = element.children.length;
     document.querySelector(".number").innerHTML = numberofChildren + " in total";
 }
-/*function EnterDeleteButton (ShowButton): void {
-   console.log("This is showing the Show Button");
-   ShowButton.classList.add("fa-trash-alt");
-}
-
-function LeaveDeleteButton (HideButton): void {
-   console.log("This is showing the Hide Button");
-   HideButton.classList.remove("fa-trash-alt");
-} */
 function CheckedButton(ClickedButton) {
     if (ClickedButton.classList.contains("fa-circle")) {
         ClickedButton.classList.remove("fa-circle");
--- Aufgabe_9/style.ts
+++ Aufgabe_9/style.ts
@@ -8,7 +8,7 @@ window.addEventListener("load", function(): void {
 function AddTaskText (): void {
     var newtask: string = (<HTMLInputElement>document.querySelector(".newtask")).value;
     console.log(newtask);
-    addTaskToList(newtask);
+    addTaskToList(newtask); // Aufruf der Funktion drunter
     // Counter:
     var element = document.querySelector(".Rahmen");
     var numberofChildren = element.children.length;
@@ -20,36 +20,26 @@ function AddTaskText (): void {
 function addTaskToList (newTaskValue: string): void {
     var addElement = document.createElement("div");
     addElement.setAttribute("class", "Template");
-    addElement.setAttribute("id", "TaskId" + taskcounter);
-    var myHtmlTemplate: string =  "";
+    addElement.setAttribute("id", "TaskId" + taskcounter); // Template konstruiert
+    var myHtmlTemplate: string =  ""; // Template Inhalte
     myHtmlTemplate += "<button onclick=\"CheckedButton(this)\" class=\"far fa-circle checkbox\" id=\"circle" + taskcounter + "\"></button>";
     myHtmlTemplate += "<input class=\"firsttask\" id=\"firsttask" + taskcounter + "\" value=\"" + newTaskValue + "\" ></input>";
     myHtmlTemplate += "<button onclick=\"DeleteTaskText(this)\" class=\"far fa-trash-alt trash\" id=\"delete" + taskcounter + "\"></button>";
     addElement.innerHTML = myHtmlTemplate;
-    document.querySelector(".Rahmen").appendChild (addElement);
+    document.querySelector(".Rahmen").appendChild (addElement); // Hinzufügen vom Template
     console.log ("addTasktoList:" + newTaskValue);
 
 }
 
 function DeleteTaskText (ClickedTrash): void {
     console.log("This is deleating the task");
-    ClickedTrash.parentElement.remove();
+    ClickedTrash.parentElement.remove(); // div wird bei click auf Tonne gelöscht
     taskcounter --;
     // Counter
     var element = document.querySelector(".Rahmen");
     var numberofChildren = element.children.length;
     document.querySelector(".number").innerHTML = numberofChildren + " in total";
 }
-
- /*function EnterDeleteButton (ShowButton): void {
-    console.log("This is showing the Show Button");
-    ShowButton.classList.add("fa-trash-alt");
-}
-
-function LeaveDeleteButton (HideButton): void {
-    console.log("This is showing the Hide Button");
-    HideButton.classList.remove("fa-trash-alt");
-} */
 
 function CheckedButton (ClickedButton): void {
    if ( ClickedButton.classList.contains("fa-circle") ) {
